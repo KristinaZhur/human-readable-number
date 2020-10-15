26c6f6314b5cb9b28dbf6288ac284759bb3838a7
Kristina Zhuravleva <krist-y_92@mail.ru>
2020-10-15 10:46:36 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function toReadable (number) {
-  
+    if (!number || number === 0) return 'zero';
+    let result = '', dig = 0;
+
+    const digits = ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen','twenty'];
+    if ( number >= 100) {
+          dig = Math.floor (number/100);
+          result =`${digits[dig]} hundred`;
+          number -= (dig*100);
+    }
+
+    if ( number > 19  ) {
+        const tens = ['','ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+        dig = Math.floor (number/10);
+        number -= (dig*10);
+        result =`${result} ${tens[dig]} ${digits[number]}`;
+    }  else {
+            result =`${result} ${digits[number]}`;
+        }
+        return result.trim();
 }
